4059e255c0c716c5b5d22e71283bd28c2b075065
Santi Hernández <santi@santihdzs.com>
2026-03-12 16:09:23 +0000

    Reset to octavio's original config with tokyonight theme

--- stow/common/nvim/lua/custom/chadrc.lua
+++ stow/common/nvim/lua/custom/chadrc.lua
@@ -1,13 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
--- Theme: poimandres (default), toggle between poimandres and tokyonight
--- Use :ToggleTheme command or <leader>tt after starting nvim
-M.ui = {
-  theme = 'onedark',
-  theme_toggle = { 'onedark', 'tokyonight' }
-}
-
+M.ui = { theme = 'tokyonight' }
 M.plugins = "custom.plugins"
 
 return M
