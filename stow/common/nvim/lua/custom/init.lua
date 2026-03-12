4b1f79d88b5a818fc1b7e2a4eb377ab65867bf46
Santi Hernández <santi@santihdzs.com>
2026-03-12 16:02:34 +0000

    Fix: remove keymap from custom/init.lua causing buffer issues

--- stow/common/nvim/lua/custom/init.lua
+++ stow/common/nvim/lua/custom/init.lua
@@ -1,5 +1,2 @@
 vim.opt.relativenumber = true
 vim.opt.autowriteall = true
-
--- Theme toggle: switch between poimandres and tokyonight
-vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>lua require("base46").toggle_theme()<CR>', { noremap = true, silent = true, desc = 'Toggle theme (poimandres/tokyonight)' })
