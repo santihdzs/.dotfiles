882ee4cc33bda473b9c1f12bc61c3e2c63074757
Santi Hernández <santi@santihdzs.com>
2026-03-12 16:00:38 +0000

    Fix: use onedark instead of poimandres (not available in base46 v2.0)

--- stow/common/nvim/lua/custom/chadrc.lua
+++ stow/common/nvim/lua/custom/chadrc.lua
@@ -4,8 +4,8 @@ local M = {}
 -- Theme: poimandres (default), toggle between poimandres and tokyonight
 -- Use :ToggleTheme command or <leader>tt after starting nvim
 M.ui = {
-  theme = 'poimandres',
-  theme_toggle = { 'poimandres', 'tokyonight' }
+  theme = 'onedark',
+  theme_toggle = { 'onedark', 'tokyonight' }
 }
 
 M.plugins = "custom.plugins"
