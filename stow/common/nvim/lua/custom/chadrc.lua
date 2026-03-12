48ce862cf0b2238dfe35aa71fdfeff1256b05640
Santi Hernández <santi@santihdzs.com>
2026-03-12 15:54:06 +0000

    Set up octavio's neovim config with poimandres theme and toggle to tokyonight
    - Downloaded full nvim config from octavio-navarro/config_files
    - Updated base46 to v3.0 for poimandres support
    - Set poimandres as default theme in chadrc.lua
    - Added theme toggle with <leader>tt (poimandres <-> tokyonight)
    - Updated lazy-lock.json and added missing config files

--- stow/common/nvim/lua/custom/chadrc.lua
+++ stow/common/nvim/lua/custom/chadrc.lua
@@ -1,9 +1,13 @@
 ---@type ChadrcConfig
 local M = {}
 
+-- Theme: poimandres (default), toggle between poimandres and tokyonight
+-- Use :ToggleTheme command or <leader>tt after starting nvim
 M.ui = {
-  theme = 'tokyonight',
+  theme = 'poimandres',
+  theme_toggle = { 'poimandres', 'tokyonight' }
 }
+
 M.plugins = "custom.plugins"
 
 return M
--- stow/common/nvim/lua/custom/init.lua
+++ stow/common/nvim/lua/custom/init.lua
@@ -1,2 +1,5 @@
 vim.opt.relativenumber = true
 vim.opt.autowriteall = true
+
+-- Theme toggle: switch between poimandres and tokyonight
+vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>lua require("base46").toggle_theme()<CR>', { noremap = true, silent = true, desc = 'Toggle theme (poimandres/tokyonight)' })
